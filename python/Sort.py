62cbc174ed9ef6f28e05bfeefe52f805d008e0f5
zhtsh <zhtsh.lichao@gmail.com>
2019-11-09 21:14:54 +0800

    add quick sort

--- python/Sort.py
+++ python/Sort.py
@@ -63,9 +63,31 @@ def insert_sort(data_list):
     print(data_list)
 
 
+def quick_sort(data_list, left, right):
+    if left < right:
+        p = partition(data_list, left, right)
+        quick_sort(data_list, left, p)
+        quick_sort(data_list, p+1, right)
+
+
+def partition(data_list, left, right):
+    pivot = data_list[(left + right) / 2]
+    i = left
+    j = right
+    while True:
+        while data_list[i] < pivot:
+            i += 1
+        while data_list[j] > pivot:
+            j -= 1
+        if i >= j:
+            return j
+        data_list[i], data_list[j] = data_list[j], data_list[i]
+
+
 if __name__ == '__main__':
     n = int(sys.argv[1])
     data_list = [random.randint(1, 10000) for i in range(n)]
+    # data_list.sort(reverse=True)
     print("source data list: ")
     print(data_list)
     select_data_list = list(data_list)
@@ -76,4 +98,12 @@ if __name__ == '__main__':
     print("")
     insert_data_list = list(data_list)
     insert_sort(insert_data_list)
+    print("")
+    quick_data_list = list(data_list)
+    print("starting quick sorting........")
+    begin_time = time.time()
+    quick_sort(quick_data_list, 0, n-1)
+    print("elasped time: {} seconds".format(time.time()-begin_time))
+    print("quick sort result: ")
+    print(quick_data_list)
     print("")
